d9061351ee7495c8b78f73ed2ab1262bfa8124ac
Eduardo N Gomes <Eduardo.000@live.com>
2022-11-08 17:09:56 -0300

    add back-end

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://foodexplorer-api-eduardo.herokuapp.com/'
+  baseURL: 'https://foodexplorer-api-eduardo.herokuapp.com'
 })
